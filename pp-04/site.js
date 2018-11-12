7d8ae085240fc9814c8e1db971fb7ee643741027
Bryan <bcruzcastillo@hawk.iit.edu>
2018-11-12 12:25:59 -0600

    Add DOM loader/finder. Implement javascript for the doorbell function.

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,33 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+// Use javascript to modify the DOM
+// use IIFE (Immediately Invoked Function Expression)
+(function(){
+  if (typeof(document.querySelector)==="undefined") {
+    // if document.querySelector is undefined, then just
+    // return and get out
+    return;
+  }// else below
+  // being unobstrusive JavaScript Below...
+  document.addEventListener('DOMContentLoaded', function() {
+    // Pretty much all of our page's JavaScript that works
+    // on the DOM will be written in here...
+    var vElement = document.querySelector('#doorbell');
+    // Add a js class on the <html> element...
+    document.querySelector('html').className = 'js';
+
+    function ringDoorbell() {
+      var doorbell = new Audio('media/doorbell.mp3');
+      doorbell.play();
+    }
+
+    document.querySelector('audio').className = 'hide';
+
+    // Listen for click events on the #doorbell...
+    vElement.addEventListener('click', function() {
+      // When it's clicked, run ringDoorbell
+
+      ringDoorbell();
+    });// end vElement Event Listener
+
+  });// end document event listener
+
+})();// end IIFE
